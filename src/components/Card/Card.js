edbd8570dab1c8e381896bdb0baefce305fc9ef2
Y0K4I <skoralex02@gmail.com>
2020-08-06 00:53:47 +0300

    cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import './Card.css'
+import './Card.css';
+import styled from 'styled-components';
+
+const Sprite = styled.img`
+    width: 100px;
+    height: 100px;
+`;
 
 class Card extends Component {
+    state = {
+        name: '',
+        imgUrl: '',
+        pokemonIndex: '',
+        imgLoading: true,
+        toManyRequests: false
+    }
+
+    componentDidMount() {
+        const name = this.props.name;
+        const url = this.props.url;
+        const pokemonIndex = url.split("/")[url.split('/').length - 2]
+        const imgUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`
+
+        this.setState({
+            name,
+            imgUrl,
+            pokemonIndex,
+
+        });
+    }
+
     render() {
+        
         return (
             <div className="cards-block_card">
                 <div className="cards-block_card-top">
-        
+                    <div className="cards-block_card-top_index">{this.state.pokemonIndex}</div>
+                    <div className="cards-block_card-top_name">{this.state.name.toLowerCase().split(' ').map(letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                    .join(' ')}</div>
                 </div>
                 <div className="cards-block_card-bottom">
-        
+                    <Sprite 
+                    className="cards-block_card-bottom_img" 
+                    src={this.state.imgUrl}
+                    onLoad={() => this.setState({imgLoading: false})}
+                    onError={() => this.setState({toManyRequests: true})}
+                    />
+                    
                 </div>
             </div>
         )
--- src/components/Card/Cards_block.js
+++ src/components/Card/Cards_block.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import './Card.css'
-import Card from './Card'
+import './Card.css';
+import Card from './Card';
+import axios from 'axios';
 
 class Cards_block extends Component {
+    state = {
+        url: "https://pokeapi.co/api/v2/pokemon?limit=964",
+        pokemon: null
+    };
+
+    async componentDidMount() {
+        const res = await axios.get(this.state.url);
+        this.setState({ pokemon: res.data['results'] })
+    }
+
     render() {
         return(
-            <div className="cards-block">
-                <Card />
+            <div>
+                {this.state.pokemon ? (<div className="cards-block">
+                {this.state.pokemon.map(pokemon => (
+                    <Card 
+                        name={pokemon.name}
+                        url={pokemon.url}
+                        key={pokemon.name}
+                    />
+                ))}
+            </div>) : (<h1>Wait!</h1>)}
             </div>
-        )
+        );
     }
 }
 
