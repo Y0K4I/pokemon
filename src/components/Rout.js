9de31d518fa61297050fa5eb175585cdade32398
Y0K4I <skoralex02@gmail.com>
2020-08-10 00:13:23 +0300

    rout-working

--- src/components/Rout.js
+++ src/components/Rout.js
@@ -2,22 +2,18 @@ import React, { Component } from "react";
 
 const pokemonIndex = 1;
 
-export class PokeList extends Component {
-    onclick () {
+export function PokeList() {
+    const onclick = () => {
       window.location.assign('http://localhost:3000/#/PokeList/');
     }
 
-    render() {
-      return (<a onClick={(e) => this.onclick(e)}><span>PokeList</span></a>);
-    } 
-  }
+    return (<a onClick={() => onclick()}><span>PokeList</span></a>);
+  } 
 
-export class Pokedex extends Component {
-    onclick () {
+export function Pokedex() {
+    const onclick = () => {
       window.location.assign(`http://localhost:3000/#/PokeList/pokemon/${pokemonIndex}`);
     }
     
-    render() {
-      return (<a  onClick={(e) => this.onclick(e)}><span>Pokedex</span></a>);
-    }
+    return (<a  onClick={() => onclick()}><span>Pokedex</span></a>);
   }
