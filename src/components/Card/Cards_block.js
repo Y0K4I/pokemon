cc82a6b985d6eb652d92e575bd0956a487bac052
Y0K4I <skoralex02@gmail.com>
2020-08-14 23:27:12 +0300

    rope

--- src/components/Card/Cards_block.js
+++ src/components/Card/Cards_block.js
@@ -3,46 +3,82 @@ import './Card.css';
 import Card from './Card';
 import { apiGetLimited, apiGetAll } from '../redux/apiGet';
 import Pagination from '../Pagination/Pagination';
+import styled from 'styled-components';
+import ropeEnd from './img/rope-end.png'
+import ropeBody from './img/rope-body.png'
 
 export default function Cards_block() {
-    const [limit, setLimit] = useState(30)
+    // let defaultLimit = 30
+    const [limit, setLimit] = useState(10)
     const [offset, setOffset] = useState(0)
     const [pokemon, setPokemon] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
     const [allPokemons, setAllPokemons] = useState('')
 
+
+    const getNewLimit = () => {
+        setLimit(limit+5)
+    }
+    
     useEffect(() => {
-        apiGetLimited(limit, offset).then(result=> {
+        apiGetLimited(limit, offset).then(result => {
             setPokemon(result.data.results)
             setAllPokemons(result.data.count)
         })
-    }, [offset])
-
-    useEffect(() => {
         if(currentPage)
         setOffset(currentPage*limit-limit)
-    }, [currentPage])
-    
+    }, [offset, limit, currentPage])
+
     const paginate = (pageNumber) =>  setCurrentPage(pageNumber)
-    
+
+    const nextPage = currentPage+1
+    const prevPage = currentPage-1
 
     return(
         <div>
-            {!!pokemon.length && console.log(pokemon)}
-            {!!pokemon.length ? (<div className="cards-block">
-            {pokemon.map(pokemon => (
-                <Card 
-                    name={pokemon.name} 
-                    url={pokemon.url}
-                    key={pokemon.name}
-                />
-            ))}
-        </div>) : (<h2>Wait!</h2>)}
-            <Pagination 
+            <Block>
+                {!!pokemon.length}
+                {!!pokemon.length ? (<div className="cards-block">
+                {pokemon.map(pokemon => (
+                    <Card 
+                        name={pokemon.name} 
+                        url={pokemon.url}
+                        key={pokemon.name}
+                    />
+                ))}
+            </div>) : (<h2>Wait!</h2>)}
+            <Rope>
+                <RopeBody src={ropeBody}/>
+                <img src={ropeEnd} alt='rope-end' style={{
+                    height: '100px'
+                }} onClick={() => getNewLimit()}></img>
+            </Rope>
+            </Block>
+            <Pagination
+                currentPage={currentPage}
                 pokemonsPerPage={limit}
-                totalPokemons={allPokemons}
+                totalPokemons = {!!allPokemons ? allPokemons : 0}
                 paginate={paginate}
+                nextPage={nextPage}
+                prevPage={prevPage}
             />
         </div>
     );
 }
+
+const Block = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+const Rope = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const RopeBody = styled.img`
+    height: 150px;
+    width: 10px;
+    margin: 0px 0px 0px 15px;
+    @keyframes box {
+        from
+    }
+`
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,25 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Pagination.css'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
 
-export default function Pagination({pokemonsPerPage, totalPokemons, paginate}) {
-    const pageNumbers = []
+export default function Pagination({pokemonsPerPage, totalPokemons, paginate, currentPage, prevPage, nextPage}) {
+    const [pageNumbers, setPageNumber] = useState([]) 
+    const [totalPages, setTotalPages] = useState(totalPokemons / pokemonsPerPage)
+    const maxPage = Math.floor(totalPages)
 
-    for(let i=1; i <= Math.ceil(totalPokemons / pokemonsPerPage); i++) {
-        pageNumbers.push(i)
-    }
-
-    debugger
+    useEffect(() => {
+        if(totalPokemons) setTotalPages(totalPokemons / pokemonsPerPage)
+    }, [totalPokemons])
+    
+    useEffect(() => {
+        console.log({currentPage, totalPages, totalPokemons , pokemonsPerPage})
+        if(!totalPages) return;
+        setPageNumber(new Array(9).fill(currentPage).map((num, ind) => {
+            const current = currentPage>5?currentPage-4+ind:ind+1
+            if(current>maxPage) return;
+            return current
+        }))
+    }, [totalPages, currentPage])
 
     return (
-        <nav>
+    <nav>
+        <PaginationBlock>
+            <Button onClick={() => {
+                paginate(prevPage)
+                }} disabled={currentPage === 1}>prev</Button>
             <ul className="pagination">
-                {pageNumbers.map(number => (
+                {pageNumbers.length && pageNumbers.map(number => (
                     <li key={number} className="pagination-list">
-                        <Link to="/PokeList" className="pagination-list_link" onClick={() => paginate(number)}>{number}</Link>
+                        <Link to="/PokeList" className={`pagination-list_link 
+                        ${number===currentPage?'pagination-list_link-clicked':'pagination-list_link'}`} onClick={() => {
+                            paginate(number)}}>{number}</Link>
                     </li>
                 ))}
             </ul>
-        </nav>
+            <Button onClick={() => paginate(nextPage)}disabled={currentPage === maxPage}>next</Button>
+        </PaginationBlock>
+    </nav>
     )
 }
+
+const PaginationBlock = styled.nav`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const Button = styled.button``
